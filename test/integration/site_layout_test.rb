36ee5ccce910dc763d478bf16ae01535f3c67921
LeeSD90 <lee.doyle90@gmail.com>
2018-04-23 13:29:31 +0200

    Add full_title helper to test suite

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,8 +5,20 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
+    get root_path
+    assert_select "title", full_title()
+
     assert_select "a[href=?]", help_path
+    get help_path
+    assert_select "title", full_title("Help")
+
     assert_select "a[href=?]", about_path
+    get about_path
+    assert_select "title", full_title("About")
+
     assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_title("Contact")
+
   end
 end
