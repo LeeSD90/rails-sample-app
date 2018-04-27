41f4b82adff21a5658c19ef729121d6c71efede4
LeeSD90 <lee.doyle90@gmail.com>
2018-04-27 11:17:22 +0200

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
-			flash[:success] = "Account created!"
+			flash[:success] = "Account successfully created!"
 			redirect_to @user
 		else
 			render 'new'
