e868c267db07cba89351f40a73113e1a028c12d5
LeeSD90 <lee.doyle90@gmail.com>
2018-04-28 11:41:29 +0200

    Fix login path

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
-  get  'login',    to: 'sessions#new'
-  get  'login',    to: 'sessions#create'
-  get  'logout',    to: 'sessions#destroy'
+  get  '/login',    to: 'sessions#new'
+  get  '/login',    to: 'sessions#create'
+  get  '/logout',    to: 'sessions#destroy'
   resources :users
   
   root 'static_pages#home'
