ecb19bdb0c0dae5fe3bcd812435cd4b926e0f4c0
LeeSD90 <lee.doyle90@gmail.com>
2018-05-03 11:26:59 +0200

    Add user profile microposts test and fix duplicate show method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@microposts = @user.microposts.paginate(page: params[:page])
+		redirect_to root_url and return unless @user.activated?
 	end
   
 	def new
@@ -14,11 +15,6 @@ class UsersController < ApplicationController
 
 	def index
 		@users = User.where(activated: true).paginate(page: params[:page])
-	end
-
-	def show
-		@user = User.find(params[:id])
-		redirect_to root_url and return unless @user.activated?
 	end
 
 	def create
