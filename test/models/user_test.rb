513274c51931064a2c598379f3cb2ad81f37f842
LeeSD90 <lee.doyle90@gmail.com>
2018-05-07 15:22:05 +0200

    Update user post feed to show followed posts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -94,5 +94,23 @@ class UserTest < ActiveSupport::TestCase
     bob.unfollow(archer)
     assert_not bob.following?(archer)
   end
+
+  test "feed should have the right posts" do
+    bob = users(:bob)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert bob.feed.include?(post_following)
+    end
+    # Posts from self
+    bob.microposts.each do |post_self|
+      assert bob.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not bob.feed.include?(post_unfollowed)
+    end
+  end
   
 end
